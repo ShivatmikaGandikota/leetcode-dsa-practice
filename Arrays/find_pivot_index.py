fd0d0243dd7c52941733ab374f56bfd6bc71c6b9
ShivatmikaGandikota <23MH1A0422@ACOE.EDU.IN>
2026-03-17 21:29:38 +0530

    Update find_pivot_index.py

--- Arrays/find_pivot_index.py
+++ Arrays/find_pivot_index.py
@@ -1,21 +1,12 @@
 #https://leetcode.com/problems/find-pivot-index/
 #724. Find Pivot Index
-#using Prefix Sum
 
 class Solution:
     def pivotIndex(self, nums):
-        prefix = [0]*len(nums)
-
-        prefix[0] = nums[0]
-
-        for i in range(1, len(nums)):
-            prefix[i] = prefix[i-1] + nums[i]
-
-        total = prefix[-1]
 
         for i in range(len(nums)):
-            left = prefix[i-1] if i > 0 else 0
-            right = total - prefix[i]
+            left = sum(nums[:i])
+            right = sum(nums[i+1:])
 
             if left == right:
                 return i
